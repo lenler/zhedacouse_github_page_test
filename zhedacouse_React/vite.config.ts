ea38c1c9fb600189da3ce4f048f82af47c46bd34
bule_punkin <2122151459@qq.com>
2026-04-15 11:50:06 +0800

    fix: update github pages base for fork repo

--- zhedacouse_React/vite.config.ts
+++ zhedacouse_React/vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zhedacouse/',
+  base: "/zhedacouse_github_page_test/",
   plugins: [
     react(),
     viteCompression({
       verbose: true,
       disable: false,
       threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
+      algorithm: "gzip",
+      ext: ".gz",
     }),
   ],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'antd-vendor': ['antd', '@ant-design/icons'],
-        }
-      }
-    }
-  }
-})
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "antd-vendor": ["antd", "@ant-design/icons"],
+        },
+      },
+    },
+  },
+});
